ccba36f3a45fd26d45b3bffbb2a43a1fb286c208
Warhead007 <rawit_ham@hotmail.com>
2018-04-03 23:39:17 +0700

    first commit

---
+++ src/HelloApp.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css'
+
+class HelloApp extends React.Component{
+    constructor(props){
+        super(props)
+        
+        this.state = {value: ''}
+        
+        this.handleClick = this.handleClick.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleClick(event){
+            this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event){
+        alert('This text is: ' + this.state.value)
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <div>
+            <h1>Hello React. I try to use you.</h1>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.value} onChange={this.handleClick}/>
+                <input type="submit" value="Submit"/>
+            </form>
+            <p>Text: {this.state.value}</p>
+            </div>
+        )
+    }
+}  
+export default HelloApp
