34d99f70a5097436fc9189209e90ffd1f8b5662c
Warhead007 <rawit_ham@hotmail.com>
2018-04-07 22:34:51 +0700

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HelloApp from './HelloApp';
+import HelloStateless from './HelloStateless';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HelloApp/>
+        <HelloStateless
+        firstName = "Rawit"
+        lastName = "Supatanakorn"
+        /> 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
