e7fc042e2e7a03f3710f07c6c42b3564119022ff
Warhead007 <rawit_ham@hotmail.com>
2018-04-07 22:33:44 +0700

    Add stateless component

---
+++ src/HelloStateless.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HelloStateless =({firstName,lastName}) => (
+    <div>
+        <h2>{firstName}</h2>
+        <h2>{lastName}</h2>
+    </div>
+)
+
+export default HelloStateless
